3ec74df52f5e8bf51d3806401284fb6f7bb2f1d8
brymedia <56924275+yungbrym@users.noreply.github.com>
2022-04-16 14:26:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+//  <!-- Initialize Swiper -->
+ 
+   var swiper = new Swiper(".home-slider", {
+      grabCursor:true,
+      Loop:true,
+     navigation: {
+       nextEl: ".swiper-button-next",
+       prevEl: ".swiper-button-prev",
+     },
+   });
